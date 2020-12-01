5b279a3208c299f1661b23ffe9fdd3e7660564e7
Md. Jubayer <mdjubayer247@gmail.com>
2020-12-02 02:39:56 +0600

    speaking clock and alarm

--- app/src/main/java/help/codered/focusaware/MainActivity.java
+++ app/src/main/java/help/codered/focusaware/MainActivity.java
@@ -4,51 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
+import android.app.Notification;
+import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.judemanutd.autostarter.AutoStartPermissionHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_update_interval,btn_stop_alarm;
+    Button btn_update_interval,
+            btn_stop_alarm,
+            btn_check_autostart;
     EditText edit_text_interval;
     public static Uri notification;
     public static Ringtone r;
+    private static String TAG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TAG=getString(R.string.uuid_for_worker);
         ////////////////////////////////////////////////////////////////////////
         btn_stop_alarm=(Button)findViewById(R.id.btn_stop_alarm);
         btn_update_interval=(Button)findViewById(R.id.btn_save_interval);
+        btn_check_autostart=(Button)findViewById(R.id.btn_check_autostart);
+        edit_text_interval=(EditText)findViewById(R.id.edit_text_interval);
 
         ////////////////////////////////////////////////////////////////////
-        edit_text_interval=(EditText)findViewById(R.id.edit_text_interval);
 
         notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-       // r.play();
-
-
-//
         Editor editor= (Editor) getPreferences(MODE_PRIVATE).edit();
         final int[] interval = {getPreferences(MODE_PRIVATE).getInt("interval", 0)};
         if(interval[0] ==0){
             editor.putInt("interval",15);
             editor.commit();
+            WorkManager workManager=WorkManager.getInstance(this);
+            workManager.cancelAllWorkByTag(TAG);
             PeriodicWorkRequest saveRequest =
                     new PeriodicWorkRequest.Builder(sheduleWork.class, 15, TimeUnit.MINUTES)
-                            // Constraints
+                            .addTag(TAG)
                             .build();
 
             WorkManager.getInstance(this).enqueue(saveRequest);
-
         }
         interval[0] =getPreferences(MODE_PRIVATE).getInt("interval",0);
         edit_text_interval.setText(""+ interval[0]);
@@ -61,12 +72,13 @@ public class MainActivity extends AppCompatActivity {
                 edit_text_interval.setText(newInterval+"");
                 editor.putInt("interval",newInterval);
                 editor.commit();
-            PeriodicWorkRequest saveRequest =
+                WorkManager workManager=WorkManager.getInstance(MainActivity.this);
+                workManager.cancelAllWorkByTag(TAG);
+                PeriodicWorkRequest saveRequest =
                     new PeriodicWorkRequest.Builder(sheduleWork.class, newInterval, TimeUnit.MINUTES)
-                            // Constraints
+                            .addTag(TAG)
                             .build();
-
-            WorkManager.getInstance(MainActivity.this).enqueue(saveRequest);
+                WorkManager.getInstance(MainActivity.this).enqueue(saveRequest);
             }
         });
 
@@ -79,6 +91,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_check_autostart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAutoStart(true);
+            }
+        });
+        checkAutoStart(false);
     }
 
+
+    private void checkAutoStart(boolean force_check){
+        boolean auto_start_availability;
+        boolean permission_success;
+        if(force_check){
+            auto_start_availability = AutoStartPermissionHelper.getInstance().isAutoStartPermissionAvailable(this);
+            permission_success = AutoStartPermissionHelper.getInstance().getAutoStartPermission(this);
+            showAutoStartToast(auto_start_availability,permission_success);
+        }
+        else {
+            boolean checked;
+            checked=getPreferences(MODE_PRIVATE).getBoolean("auto_start_check",false);
+            if(!checked){
+                auto_start_availability = AutoStartPermissionHelper.getInstance().isAutoStartPermissionAvailable(this);
+                permission_success = AutoStartPermissionHelper.getInstance().getAutoStartPermission(this);
+                SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();  // Put the values from the UI
+                editor.putBoolean("auto_start_check",true);
+                // Commit to storage
+                editor.apply();
+                showAutoStartToast(auto_start_availability,permission_success);
+            }
+        }
+    }
+
+    public void showAutoStartToast(boolean auto_start_availability, boolean permission_success){
+        if(auto_start_availability){
+            Toast.makeText(MainActivity.this, "autostart available", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "autostart not available", Toast.LENGTH_SHORT).show();
+        }
+        if(permission_success){
+            Toast.makeText(MainActivity.this, "Permission success", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Auto start permission feature not available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TAG=getString(R.string.uuid_for_worker);
+    }
 }
--- app/src/main/java/help/codered/focusaware/sheduleWork.java
+++ app/src/main/java/help/codered/focusaware/sheduleWork.java
@@ -2,19 +2,27 @@ package help.codered.focusaware;
 
 
 import android.app.Activity;
+import android.app.Notification;
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import static help.codered.focusaware.MainActivity.r;
-
 public class sheduleWork extends Worker {
-
     private Context context;
+
+    private TextToSpeech textToSpeechSystem;
+
     public sheduleWork(
             @NonNull Context context,
             @NonNull WorkerParameters params) {
@@ -24,20 +32,37 @@ public class sheduleWork extends Worker {
 
     @Override
     public Result doWork() {
+        textToSpeechSystem = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    UtteranceProgressListener utteranceProgressListener=new UtteranceProgressListener() {
+                        @Override
+                        public void onStart(String utteranceId) {
+                            Toast.makeText(context,"Focus Aware, TTS-S",Toast.LENGTH_SHORT).show();
+                        }
 
-        // Do the work here--in this case, upload the images.
-       // uploadImages();
-        r.play();
-//        Button btn_stop_alarm=(Button) ((Activity)getApplicationContext()).findViewById(R.id.btn_stop_alarm);
-//        btn_stop_alarm.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(r.isPlaying()){
-//                    r.stop();
-//                }
-//            }
-//        });
-        // Indicate whether the work finished successfully with the Result
+                        @Override
+                        public void onDone(String utteranceId) {
+                            r.play();
+                            Toast.makeText(context,"Focus Aware, TTS-D",Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(String utteranceId) {
+                            r.play();
+                            Toast.makeText(context,"Focus Aware, TTS-E",Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                    textToSpeechSystem.setOnUtteranceProgressListener(utteranceProgressListener);
+                    textToSpeechSystem.setSpeechRate(.9f);
+                    String mUtteranceId=NotificationID.getID()+"";
+                    String currentTime = new SimpleDateFormat("HH mm a", Locale.getDefault()).format(new Date());
+                    textToSpeechSystem.speak(currentTime, TextToSpeech.QUEUE_FLUSH, null,mUtteranceId );
+                }
+            }
+        });
+        Toast.makeText(context,"Focus Aware",Toast.LENGTH_SHORT).show();
         return Result.success();
     }
 }
